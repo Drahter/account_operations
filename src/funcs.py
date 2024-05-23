6036e114d9a7dc894e18350c42ec2c3225077864
Dmit Bashmakov <denegniy1@gmail.com>
2024-05-23 15:37:59 +0500

    added get_data function

--- src/funcs.py
+++ src/funcs.py
@@ -1,5 +1,10 @@
 import json
+import os
 
 
 def get_data():
-    pass
+    with open(os.path.dirname(__file__) + '/../operations.json', 'r', encoding='utf8') as file:
+        data_json = file.read()
+        data = json.loads(data_json)
+
+    return data
