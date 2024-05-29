257d76579842414910dde570fb68ab61fd4543fb
Dmit Bashmakov <denegniy1@gmail.com>
2024-05-29 11:08:01 +0500

    added get_payers_hidden function

--- src/funcs.py
+++ src/funcs.py
@@ -31,8 +31,47 @@ def get_five_latest(operations):
     for k, v in operations_with_date_sorted.items():
         operations_with_date_sorted[k] = v.strftime('%d.%m.%Y')
     for k, v in operations_with_date_sorted.items():
-        id_with_correct_date.append({'id': k, 'data': v})
+        id_with_correct_date.append({'id': k, 'date': v})
 
     five_latest = id_with_correct_date[0:5]
 
     return five_latest
+
+
+def get_payers_hidden(action_id, operations):
+    for each in operations:
+        if each['id'] == action_id:
+            from_to = ''
+            if 'from' in each.keys():
+                from_ls = each['from'].split()
+                for part in from_ls:
+                    if part.isalpha():
+                        from_to += part + ' '
+
+                    elif len(part) == 16:
+                        card_number = part[0:4] + ' ' + part[4:6] + '** ****' + ' ' + part[12:16]
+                        from_to += card_number
+
+                    else:
+                        check_number = '**' + part[-4:]
+                        from_to += check_number
+            else:
+                from_to += 'Новый счет'
+
+            from_to += ' -> '
+
+            if 'to' in each.keys():
+                to_ls = each['to'].split()
+                for part in to_ls:
+                    if part.isalpha():
+                        from_to += part + ' '
+
+                    elif len(part) == 16:
+                        card_number = part[0:4] + ' ' + part[4:6] + '** ****' + ' ' + part[12:16]
+                        from_to += card_number
+
+                    else:
+                        check_number = '**' + part[-4:]
+                        from_to += check_number
+
+            return from_to
