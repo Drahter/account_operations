7fc1040169533ca91394553eee2bf9e292e85afd
Dmit Bashmakov <denegniy1@gmail.com>
2024-05-29 09:47:35 +0500

    test for get_time added

--- tests/test_funcs.py
+++ tests/test_funcs.py
@@ -4,3 +4,56 @@ from src.funcs import *
 def test_get_data():
     assert type(get_data()) == list
 
+
+def test_get_time():
+    data_1 = [
+      {
+        "id": 441945886,
+        "state": "EXECUTED",
+        "date": "2019-08-26T10:50:58.294041",
+        "operationAmount": {
+          "amount": "31957.58",
+          "currency": {
+            "name": "руб.",
+            "code": "RUB"
+          }
+        },
+        "description": "Перевод организации",
+        "from": "Maestro 1596837868705199",
+        "to": "Счет 64686473678894779589"
+      },
+      {
+        "id": 41428829,
+        "state": "EXECUTED",
+        "date": "2019-07-03T18:35:29.512364",
+        "operationAmount": {
+          "amount": "8221.37",
+          "currency": {
+            "name": "USD",
+            "code": "USD"
+          }
+        },
+        "description": "Перевод организации",
+        "from": "MasterCard 7158300734726758",
+        "to": "Счет 35383033474447895560"
+      }
+    ]
+    data_2 = [
+      {
+        "id": 441945886,
+        "state": "EXECUTED",
+        "date": "2019-08-26T10:50:58.294041",
+        "operationAmount": {
+          "amount": "31957.58",
+          "currency": {
+            "name": "руб.",
+            "code": "RUB"
+          }
+        },
+        "description": "Перевод организации",
+        "from": "Maestro 1596837868705199",
+        "to": "Счет 64686473678894779589"
+      }
+    ]
+    assert get_time(data_1) == {441945886: "2019-08-26", 41428829: "2019-07-03"}
+    assert get_time(data_2) == {441945886: "2019-08-26"}
