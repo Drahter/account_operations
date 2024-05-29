ec8cd219bc810233bfeafee9af3bb797863e3edf
Dmit Bashmakov <denegniy1@gmail.com>
2024-05-29 09:48:05 +0500

    get_time function added

--- src/funcs.py
+++ src/funcs.py
@@ -8,3 +8,14 @@ def get_data():
         data = json.loads(data_json)
 
     return data
+
+
+def get_time(operations):
+    id_with_time = {}
+    for each in operations:
+        if "state" in each.keys():
+            if each["state"] != "CANCELED":
+                if len(each.keys()) != 0:
+                    id_with_time[each["id"]] = each["date"][0:10]
+
+    return id_with_time
