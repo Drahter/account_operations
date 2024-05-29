e1d2058b17b0a4768ea910f274a284f2036231f3
Dmit Bashmakov <denegniy1@gmail.com>
2024-05-29 10:30:17 +0500

    get_five_latest function added

--- src/funcs.py
+++ src/funcs.py
@@ -1,5 +1,6 @@
 import json
 import os
+from datetime import date
 
 
 def get_data():
@@ -19,3 +20,19 @@ def get_time(operations):
                     id_with_time[each["id"]] = each["date"][0:10]
 
     return id_with_time
+
+
+def get_five_latest(operations):
+    id_with_date = {}
+    id_with_correct_date = []
+    for k, v in operations.items():
+        id_with_date[k] = date.fromisoformat(v)
+    operations_with_date_sorted = dict(sorted(id_with_date.items(), key=lambda item: item[1], reverse=True))
+    for k, v in operations_with_date_sorted.items():
+        operations_with_date_sorted[k] = v.strftime('%d.%m.%Y')
+    for k, v in operations_with_date_sorted.items():
+        id_with_correct_date.append({'id': k, 'data': v})
+
+    five_latest = id_with_correct_date[0:5]
+
+    return five_latest
