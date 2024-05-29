a7adf2e9a23ef6426b3159f7231ee96c1c71e978
Dmit Bashmakov <denegniy1@gmail.com>
2024-05-29 11:07:41 +0500

    added test for get_payers_hidden function

--- tests/test_funcs.py
+++ tests/test_funcs.py
@@ -1,5 +1,8 @@
 from src.funcs import *
 
+
+data = get_data()
+
 data_1 = [
   {
     "id": 441945886,
@@ -47,7 +50,8 @@ data_2 = [
     "description": "Перевод организации",
     "from": "Maestro 1596837868705199",
     "to": "Счет 64686473678894779589"
-  }
+  },
+  {"date": "2019-07-03T18:35:29.512364", "description": "Перевод организации"}
 ]
 
 
@@ -67,14 +71,18 @@ def test_get_time():
 
 
 def test_get_five_latest():
-    assert get_five_latest(id_with_time_1) == [{'id': 441945886, 'data': '26.08.2019'}, {'id': 41428829,
-                                                                                         'data': '03.07.2019'}]
-    assert get_five_latest(id_with_time_2) == [{'id': 414244455, 'data': '06.03.2022'}, {'id': 441945886,
-                                                                                         'data': '26.08.2019'}]
-    assert get_five_latest(id_with_time_3) == [{'id': 1412122222, 'data': '12.03.2093'},
-                                               {'id': 141244444, 'data': '15.05.2022'},
-                                               {'id': 441945886, 'data': '26.08.2019'},
-                                               {'id': 41428829, 'data': '03.07.2019'},
-                                               {'id': 44194582323, 'data': '23.02.2001'}]
+    assert get_five_latest(id_with_time_1) == [{'id': 441945886, 'date': '26.08.2019'}, {'id': 41428829,
+                                                                                         'date': '03.07.2019'}]
+    assert get_five_latest(id_with_time_2) == [{'id': 414244455, 'date': '06.03.2022'}, {'id': 441945886,
+                                                                                         'date': '26.08.2019'}]
+    assert get_five_latest(id_with_time_3) == [{'id': 1412122222, 'date': '12.03.2093'},
+                                               {'id': 141244444, 'date': '15.05.2022'},
+                                               {'id': 441945886, 'date': '26.08.2019'},
+                                               {'id': 41428829, 'date': '03.07.2019'},
+                                               {'id': 44194582323, 'date': '23.02.2001'}]
 
 
+def test_get_payers_hidden():
+    assert get_payers_hidden(441945886, data_1) == 'Maestro 1596 83** **** 5199 -> Счет **9589'
+    assert get_payers_hidden(441945886, data) == 'Maestro 1596 83** **** 5199 -> Счет **9589'
+    assert get_payers_hidden(716496732, data) == 'Visa Gold 5999 41** **** 6353 -> Счет **4472'
